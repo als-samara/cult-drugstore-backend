5bb94a8c7c2bd2ac1eefa98602d550875ba0fd21
als-samara <samaraalmeida379@gmail.com>
2024-01-04 10:11:20 -0300

    first commit

---
+++ src/main/java/com/generation/cultdrugstore/CultdrugstoreApplication.java
@@ -0,0 +1,13 @@
+package com.generation.cultdrugstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultdrugstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultdrugstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/cultdrugstore/CultdrugstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.cultdrugstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultdrugstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
